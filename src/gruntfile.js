d915aa134a29b2a79c6083dd8e0cdac1cf27ce08
De Fong <de_fong2k@yahoo.com>
2018-05-23 16:12:22 +0100

    Added basic grunt and sass configurations

---
+++ src/gruntfile.js
@@ -0,0 +1,38 @@
+﻿/// <binding AfterBuild='default' />
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Sass
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed',
+                  includePaths: [
+                    'node_modules/govuk_frontend_toolkit/stylesheets',
+                    'node_modules/govuk-elements-sass/public/sass',
+                ],
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true, // Recursive
+                        cwd: "Styles", // The startup directory
+                        src: ["**/*.scss"], // Source files
+                        dest: "wwwroot/css", // Destination
+                        ext: ".css" // File extension
+                    }
+                ]
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass']);
+};
