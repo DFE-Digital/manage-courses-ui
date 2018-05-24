e2f5ca92abff82ae7ca275b6c36cd9c6118e7205
De Fong <de_fong2k@yahoo.com>
2018-05-24 12:17:57 +0100

    Added the rest of the pipeline for frontend Added the copying of javascript from vendor Added the copying of images from vendor Added the copying of css from vendor

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -21,9 +21,62 @@ module.exports = function (grunt) {
                     {
                         expand: true, // Recursive
                         cwd: "Styles", // The startup directory
-                        src: ["**/*.scss"], // Source files
-                        dest: "wwwroot/css", // Destination
-                        ext: ".css" // File extension
+                        src: ['**/*.scss'], // Source files
+                        dest: 'wwwroot/css', // Destination
+                        ext: '.css' // File extension
+                    }
+                ]
+            }
+        },
+
+        copy: {
+            vendor: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'node_modules/govuk_frontend_toolkit/javascripts',
+                        src: ['**/*.js'],
+                        dest: 'wwwroot/javascripts'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/govuk_frontend_toolkit/images',
+                        src: ['**/*.png' ],
+                        dest: 'wwwroot/images'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/govuk_template_jinja/assets/stylesheets',
+                        src: ['**/*.*'],
+                        dest: 'wwwroot/css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/govuk_template_jinja/assets/javascripts',
+                        src: ['**/*.js'],
+                        dest: 'wwwroot/javascripts'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/govuk_template_jinja/assets/images',
+                        src: ['**/*.*'],
+                        dest: 'wwwroot/images'
+                    },
+                ]
+            },
+            source: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'javascripts',
+                        src: ['**/*.js'],
+                        dest: 'wwwroot/javascripts'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'images',
+                        src: ['**/*.png'],
+                        dest: 'wwwroot/images'
                     }
                 ]
             }
@@ -32,7 +85,7 @@ module.exports = function (grunt) {
 
     // Load the plugin
     grunt.loadNpmTasks('grunt-sass');
-
+    grunt.loadNpmTasks('grunt-contrib-copy');
     // Default task(s).
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'copy']);
 };
