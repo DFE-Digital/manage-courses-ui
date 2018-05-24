09d19a1362a88095f507e389dbfc761e89738d87
De Fong <de_fong2k@yahoo.com>
2018-05-24 16:29:58 +0100

    Removed leaky header info

--- src/Program.cs
+++ src/Program.cs
@@ -19,6 +19,7 @@ namespace ManageCoursesUi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>()
                 .Build();
     }
