b3630eb5d8513e9826a6d7372431b6126414a27d
Tim Abell <tim@timwise.co.uk>
2018-06-12 15:43:38 +0100

    Add an API client project
    To talk to the manage courses API.
    
    Copy-paste from the ucas-importer project

---
+++ src/api-client/ManageApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GovUk.Education.ManageCourses.ApiClient
+{
+    public class ManageApi
+    {
+        public void SendToManageCoursesApi(List<Course> courses)
+        {
+            Console.WriteLine("Posting to api...");
+            var client = new ManageCoursesApiClient();
+            var payload = new Payload { Courses = new ObservableCollection<Course>(courses) };
+            client.ImportAsync(payload).Wait();
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ src/api-client/update-schema.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -v
+curl http://localhost:6001/swagger/v1/swagger.json -o manage-courses-api-swagger.json
