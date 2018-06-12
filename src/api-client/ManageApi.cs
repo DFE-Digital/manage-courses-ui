b77bf86ede236a2bcf2c152342ed0c2c35f6acde
Tim Abell <tim@timwise.co.uk>
2018-06-12 16:50:36 +0100

    Show course list retrieved from api

--- src/api-client/ManageApi.cs
+++ src/api-client/ManageApi.cs
@@ -8,12 +8,8 @@ namespace GovUk.Education.ManageCourses.ApiClient
         public async Task<IEnumerable<Course>> GetCourses()
         {
             var client = new ManageCoursesApiClient();
-            var response = await client.ExportAsync();
-            // todo: return actual course list
-            return new List<Course>()
-            {
-                new Course {Title = "todo-course-title"},
-            };
+            var observableCollection = await client.ExportAsync();
+            return observableCollection;
         }
     }
 }
