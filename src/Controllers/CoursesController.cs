d946814c7ba5a5a8d4f8f7954e969fc1d3e7a1ad
Tim Abell <tim@timwise.co.uk>
2018-06-12 15:17:10 +0100

    Add stub Courses controller
    Redirect straight to course list from root url

---
+++ src/Controllers/CoursesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageCoursesUi.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
