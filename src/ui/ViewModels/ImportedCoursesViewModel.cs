439433244eb25007ca731026ddc810c6e044532a
De Fong <de_fong2k@yahoo.com>
2018-06-22 10:13:02 +0100

    Added imported courses page

---
+++ src/ui/ViewModels/ImportedCoursesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GovUk.Education.ManageCourses.Ui.ViewModels
+{
+    public class ImportedCoursesViewModel
+    {
+        public int TotalCount { get; set; }
+        public string TrainingProviderName { get; set; }
+    }
+}
