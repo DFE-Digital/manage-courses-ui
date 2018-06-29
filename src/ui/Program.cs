39417baa2ab7c05e6b77beee020982fe199b4537
the-daniel-rothig <daniel.rothig@gmail.com>
2018-06-29 15:59:07 +0100

    make buildable in dotnet

--- src/ui/Program.cs
+++ src/ui/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Net;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace GovUk.Education.ManageCourses.Ui
@@ -10,10 +11,28 @@ namespace GovUk.Education.ManageCourses.Ui
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options => options.AddServerHeader = false)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost(string[] args) {
+            if (System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development") 
+            {
+                return WebHost.CreateDefaultBuilder(args)
+                    .UseKestrel(options =>
+                    {
+                        options.AddServerHeader = false;
+                        options.Listen(IPAddress.Loopback, 44364, listenOptions =>
+                        {
+                            listenOptions.UseHttps("dev-cert.pfx", "manage-courses");
+                        });
+                    })
+                    .UseStartup<Startup>()
+                    .Build();
+            }
+            else
+            {
+                return WebHost.CreateDefaultBuilder(args)
+                    .UseKestrel(options => options.AddServerHeader = false)
+                    .UseStartup<Startup>()
+                    .Build();
+            }
+        }
     }
 }
