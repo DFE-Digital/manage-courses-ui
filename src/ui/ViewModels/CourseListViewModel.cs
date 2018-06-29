9b7967eed4987ed8012a073f133e3c954003fb32
Martin Bentley <martinbentley@btinternet.com>
2018-06-29 16:28:20 +0100

    Added_data_binding

---
+++ src/ui/ViewModels/CourseListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GovUk.Education.ManageCourses.ApiClient;
+
+namespace GovUk.Education.ManageCourses.Ui.ViewModels
+{
+    public class CourseListViewModel
+    {
+        public OrganisationCourses Courses { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
