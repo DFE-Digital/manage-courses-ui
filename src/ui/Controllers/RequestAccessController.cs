369e084f86114a94cbe37a37063c38cf2cd0cfb6
De Fong <de_fong2k@yahoo.com>
2018-07-10 10:25:36 +0100

    Added the confirmation for showing the confirmation for request acess

--- src/ui/Controllers/RequestAccessController.cs
+++ src/ui/Controllers/RequestAccessController.cs
@@ -1,48 +1,43 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using GovUk.Education.ManageCourses.ApiClient;
 using GovUk.Education.ManageCourses.Ui.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GovUk.Education.ManageCourses.Ui.Controllers
-{
-    public class RequestAccessController : CommonAttributesControllerBase
-    {   
+namespace GovUk.Education.ManageCourses.Ui.Controllers {
+    public class RequestAccessController : CommonAttributesControllerBase {
         private readonly ManageApi _manageApi;
-        
-        public RequestAccessController(ManageApi manageApi)
-        {
+
+        public RequestAccessController (ManageApi manageApi) {
             _manageApi = manageApi;
         }
 
         [HttpGet]
-        [Route("request-access")]
-        public ViewResult Index() 
-        {
-            return View(new RequestAccessViewModel());
+        [Route ("request-access")]
+        public ViewResult Index () {
+            return View (new RequestAccessViewModel ());
         }
 
         [HttpPost]
-        [Route("request-access")]
-        public async Task<ActionResult> Create(RequestAccessViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View("Index", model);
+        [Route ("request-access")]
+        public async Task<ActionResult> Create (RequestAccessViewModel model) {
+            if (!ModelState.IsValid) {
+                return View ("Index", model);
             }
 
-            await _manageApi.LogAccessRequest(new AccessRequest()
-            {
+            await _manageApi.LogAccessRequest (new AccessRequest () {
                 FirstName = model.FirstName,
-                LastName = model.LastName,
-                EmailAddress = model.EmailAddress,
-                Organisation = model.Organisation,
-                Reason = model.Reason,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Organisation = model.Organisation,
+                    Reason = model.Reason,
             });
 
-            return new RedirectResult("/");            
+            this.TempData["RequestAccess_To_Name"] = $"{model.FirstName} {model.LastName}";
+
+            return new RedirectResult ("/");
         }
     }
 }
