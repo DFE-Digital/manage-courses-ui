ada1fe929b9d62d666ed632d03bb2e577fb5788c
Martin Bentley <martinbentley@btinternet.com>
2018-07-18 17:15:59 +0100

    Added DataHelperTest class

---
+++ ManageCoursesUi.Tests/DataHelpersTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GovUk.Education.ManageCourses.Ui.Helpers;
+using GovUk.Education.ManageCourses.Ui.ViewModels;
+using NUnit.Framework;
+
+namespace ManageCoursesUi.Tests
+{
+    [TestFixture]
+    class DataHelpersTest
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void AgeRange_should_be_correct_for_s()
+        {
+            var param = new CourseVariantViewModel{AgeRange = "s"};
+
+            var result = param.GetAgeRange();
+
+            Assert.IsTrue(result == "Secondary (11+ years)");
+        }
+    }
+}
