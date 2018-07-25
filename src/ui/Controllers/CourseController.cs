a6bcb26c2d61938fd7d2ea04a0a1eda33278cc11
the-daniel-rothig <daniel.rothig@gmail.com>
2018-07-25 12:54:22 +0100

    actually make it build

--- src/ui/Controllers/CourseController.cs
+++ src/ui/Controllers/CourseController.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using GovUk.Education.ManageCourses.ApiClient;
 using GovUk.Education.ManageCourses.Ui.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
