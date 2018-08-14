648c3685cf83dd30108aacca10f69f632fc656f8
Tim Abell <tim@timwise.co.uk>
2018-08-14 16:37:12 +0100

    Fix handling postback when no accrediting providers exist

--- src/ui/ViewModels/OrganisationViewModel.cs
+++ src/ui/ViewModels/OrganisationViewModel.cs
@@ -3,17 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GovUk.Education.ManageCourses.Ui.ViewModels
 {
-  public class OrganisationViewModel : TabbedViewModel
-  {
-    public string InstitutionCode { get; set; }
+    public class OrganisationViewModel : TabbedViewModel
+    {
+        public OrganisationViewModel()
+        {
+            AboutTrainingProviders = new List<TrainingProviderViewModel>();
+        }
 
-    [Display(Name = "Training with you")]
-    public string TrainWithUs { get; set; }
+        public string InstitutionCode { get; set; }
 
-    [Display(Name = "About the provider (optional)")]
-    public List<TrainingProviderViewModel> AboutTrainingProviders { get; set; }
+        [Display(Name = "Training with you")]
+        public string TrainWithUs { get; set; }
 
-    [Display(Name = "Training with a disability")]
-    public string TrainWithDisability { get; set; }
-  }
+        [Display(Name = "About the provider (optional)")]
+        public List<TrainingProviderViewModel> AboutTrainingProviders { get; set; }
+
+        [Display(Name = "Training with a disability")]
+        public string TrainWithDisability { get; set; }
+    }
 }
