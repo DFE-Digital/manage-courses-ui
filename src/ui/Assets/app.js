5f46bec4fd9d78931b5f03c70966d9e9bf953a20
Dan Mitchell <dan@danmitchell.co.uk>
2018-08-16 13:27:29 +0100

    Add word count to textareas

--- src/ui/Assets/app.js
+++ src/ui/Assets/app.js
@@ -1,8 +1,15 @@
 import { initAll } from 'govuk-frontend';
+import CharCount from './Javascript/character-count.js';
 import './Javascript/cookie-bar.js';
 import './Styles/site.scss';
 
 initAll();
+
+var charCount = new GOVUK.CharCount();
+charCount.init({
+  selector: '.js-character-count',
+  wordCount: true
+})
 
 if (process.env.NODE_ENV == 'development') {
   module.hot.accept();
