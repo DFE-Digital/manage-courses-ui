136efaac78ff23f2b9ee2e006adb97b2628bef3d
De Fong <de_fong2k@yahoo.com>
2018-08-31 11:33:14 +0100

    Amended broken view models into its whole files

--- src/ui/ViewModels/BaseCourseEnrichmentViewModel.cs
+++ src/ui/ViewModels/BaseCourseEnrichmentViewModel.cs
@@ -2,30 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using GovUk.Education.ManageCourses.Ui.ViewModels.Enums;
+
 namespace GovUk.Education.ManageCourses.Ui.ViewModels
 {
-    public class FeeBasedCourseEnrichmentViewModel : BaseCourseEnrichmentViewModel
-    {
-        [Required(ErrorMessage = "Give details about the fee for UK and EU students")]
-        public decimal? FeeUkEu { get; set; }
-        public decimal? FeeInternational { get; set; }
-
-        [RegularExpression(@"^\s*(\S+\s+|\S+$){0,250}$", ErrorMessage = "Reduce the word count for fee details")]
-        public string FeeDetails { get; set; }
-
-        [RegularExpression(@"^\s*(\S+\s+|\S+$){0,250}$", ErrorMessage = "Reduce the word count for financial support")]
-
-        public string FinancialSupport { get; set; }
-    }
-
-    public class SalaryBasedCourseEnrichmentViewModel : BaseCourseEnrichmentViewModel
-    {
-
-        [RegularExpression(@"^\s*(\S+\s+|\S+$){0,250}$", ErrorMessage = "Reduce the word count for salary")]
-        [Required(ErrorMessage = "Give details about salary")]
-        public string SalaryDetails { get; set; }
-    }
-
     public abstract class BaseCourseEnrichmentViewModel
     {
         [RegularExpression(@"^\s*(\S+\s+|\S+$){0,400}$", ErrorMessage = "Reduce the word count for about this course")]
---
+++ src/ui/ViewModels/SalaryBasedCourseEnrichmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using GovUk.Education.ManageCourses.Ui.ViewModels.Enums;
+
+namespace GovUk.Education.ManageCourses.Ui.ViewModels
+{
+
+    public class SalaryBasedCourseEnrichmentViewModel : BaseCourseEnrichmentViewModel
+    {
+
+        [RegularExpression(@"^\s*(\S+\s+|\S+$){0,250}$", ErrorMessage = "Reduce the word count for salary")]
+        [Required(ErrorMessage = "Give details about salary")]
+        public string SalaryDetails { get; set; }
+    }
+}
