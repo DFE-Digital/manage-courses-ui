cca9f882c6c13055ef7ccab616f4cea85a063159
the-daniel-rothig <daniel.rothig@gmail.com>
2018-09-04 11:34:29 +0100

    add titles for enrichment pages, too

--- src/ui/Controllers/CourseController.cs
+++ src/ui/Controllers/CourseController.cs
@@ -123,8 +123,6 @@ namespace GovUk.Education.ManageCourses.Ui.Controllers
                 orgEnrichmentData?.EnrichmentModel,
                 courseEnrichmentData?.EnrichmentModel);
 
-            ViewBag.Title = $"Preview of course {ucasCourseData.Name} ({ucasCourseData.CourseCode})";
-
             return View(new SearchAndCompare.UI.Shared.ViewModels.CourseDetailsViewModel
             {
                 AboutYourOrgLink = Url.Action("About", "Organisation", new { ucasCode = instCode }),
