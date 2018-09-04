a94ed9cc92f1e10b0eca66ab1e6487fd7196ded3
Dan Mitchell <dan@danmitchell.co.uk>
2018-09-04 12:03:13 +0100

    Add redirect for legacy org details url

---
+++ src/ui/Controllers/OrganisationsController.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Education.ManageCourses.Ui.Controllers
+{
+  public class RedirectController : CommonAttributesControllerBase
+  {
+    [Authorize]
+    [Route("organisations/{ucasCode}/courses")]
+    public async Task<IActionResult> LegacyCoursePage(string ucasCode)
+    {
+      return RedirectToAction("Show", "Organisation", new { ucasCode });
+    }
+  }
+}
