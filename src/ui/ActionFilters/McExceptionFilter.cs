e4ce5a78eb3f5be91759cd5a1b7699817eaf5d87
Tim Abell <tim@timwise.co.uk>
2018-09-10 09:41:23 +0100

    Make exception filter play nicely with non-redirect error pages

--- src/ui/ActionFilters/McExceptionFilter.cs
+++ src/ui/ActionFilters/McExceptionFilter.cs
@@ -11,32 +11,24 @@ namespace GovUk.Education.ManageCourses.Ui.ActionFilters
         public void OnException(ExceptionContext context)
         {
             var swaggerException = context.Exception as SwaggerException;
-            
+
             if (swaggerException?.StatusCode >= 400 && swaggerException?.StatusCode <= 404)
             {
                 context.Result = new RedirectToActionResult("Index", "Error", new { statusCode = swaggerException?.StatusCode });
                 context.ExceptionHandled = true;
             }
-            else if (swaggerException?.StatusCode == (int) HttpStatusCode.UnavailableForLegalReasons)
+            else if (swaggerException?.StatusCode == (int)HttpStatusCode.UnavailableForLegalReasons)
             {
                 context.Result = new RedirectToActionResult("AcceptTerms", "Home", null);
                 context.ExceptionHandled = true;
             }
-            else if (swaggerException != null)
-            {                    
-                context.Result = new RedirectToActionResult("Index", "Error", new { statusCode = 500 });
-                context.ExceptionHandled = true;
-            }
-            else
+            else if (swaggerException == null)
             {
                 // not a swagger exception - so make sure AI gets notified despite us handling it here.
 
                 // MS Doc examples instantiate TelemetryClient immediately before use rather than keeping an
                 // instance around. It's not clear why but here I'm following this example.
                 new TelemetryClient().TrackException(context.Exception);
-                
-                context.Result = new RedirectToActionResult("Index", "Error", new { statusCode = 500 });
-                context.ExceptionHandled = true;
             }
         }
     }
