d96093a3d9d4b74e8b2b1814a8a5612cd9bdb538
Martin Bentley <martinbentley@btinternet.com>
2018-09-12 14:23:08 +0100

    Removed course mapper

--- src/ui/Services/ICourseMapper.cs
+++ src/ui/Services/ICourseMapper.cs
@@ -2,8 +2,8 @@ using GovUk.Education.ManageCourses.ApiClient;
 
 namespace GovUk.Education.ManageCourses.Ui.Services
 {
-    public interface ICourseMapper
-    {
-        SearchAndCompare.Domain.Models.Course MapToSearchAndCompareCourse(UcasInstitution ucasInstData, ApiClient.Course ucasCourseData, InstitutionEnrichmentModel orgEnrichmentModel, CourseEnrichmentModel courseEnrichmentModel);
-    }
+    //public interface ICourseMapper
+    //{
+    //    SearchAndCompare.Domain.Models.Course MapToSearchAndCompareCourse(UcasInstitution ucasInstData, ApiClient.Course ucasCourseData, InstitutionEnrichmentModel orgEnrichmentModel, CourseEnrichmentModel courseEnrichmentModel);
+    //}
 }
