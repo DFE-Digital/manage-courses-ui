e49d38791637cad32da183b0d82f383808ff36ad
the-daniel-rothig <daniel.rothig@gmail.com>
2018-09-13 13:59:54 +0100

    add user ids to app insights

---
+++ src/ui/SubjectTelemetryInitialiser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+
+namespace GovUk.Education.ManageCourses.Ui
+{
+    public class SubjectTelemetryInitialiser : ITelemetryInitializer
+    {
+        IHttpContextAccessor httpContextAccessor;
+
+        public SubjectTelemetryInitialiser(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated == true)
+            {
+                var foo = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+                var sub = foo.Claims.FirstOrDefault(x => x.Type == "sub");
+                if (sub != null)
+                {
+                    telemetry.Context.Properties["UserSubjectId"] = sub.Value;
+                }
+            }
+            
+        }
+    }
+}
