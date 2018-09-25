892986452687078cee01c06e7f8554f353d818c6
Dan Mitchell <dan@danmitchell.co.uk>
2018-09-25 10:01:57 +0100

    Update published success message (#241)

--- src/ui/Controllers/CourseController.cs
+++ src/ui/Controllers/CourseController.cs
@@ -79,16 +79,21 @@ namespace GovUk.Education.ManageCourses.Ui.Controllers
             if (result)
             {
                 TempData["MessageType"] = "success";
-                TempData["MessageTitle"] = "Your course has been published";
-                var searchUrl = searchAndCompareUrlService.GetCoursePageUri(course.InstCode, course.CourseCode);
+
                 if (featureFlags.ShowCourseLiveView)
                 {
+                    TempData["MessageTitle"] = "Your changes will be published within 1 working day, usually within 2 hours";
+                    var searchUrl = searchAndCompareUrlService.GetCoursePageUri(course.InstCode, course.CourseCode);
                     TempData["MessageBodyHtml"] = $@"
                         <p class=""govuk-body"">
-                            See how this course looks to applicants:
+                            The link for this course is:
                             <br />
-                            <a href='{searchUrl}'>View on website</a>
+                            <a href='{searchUrl}'>{searchUrl}</a>
                         </p>";
+                }
+                else
+                {
+                    TempData["MessageTitle"] = "Your course has been published";
                 }
             }
 
