1e3dd601be8ec68e3faef2fbba64529f5311bbd9
Theodor Vararu <theo@vararu.org>
2018-10-11 17:53:07 +0100

    Add and document basic JS unit tests
    Ported from search-and-compare-ui.

---
+++ src/ui/Assets/app.test.js
@@ -0,0 +1,49 @@
+import { initAll } from "govuk-frontend"
+import CookieMessage from "./Javascript/cookie-message"
+import BackLink from "./Javascript/back-link"
+import CharacterCount from "./Javascript/character-count"
+import serialize from "form-serialize"
+
+jest.mock("govuk-frontend")
+jest.mock("./Javascript/cookie-message")
+jest.mock("./Javascript/back-link")
+jest.mock("./Javascript/character-count")
+jest.mock("form-serialize")
+
+describe("App", () => {
+  beforeAll(() => {
+    document.body.innerHTML = `
+<div>
+  <div data-module="cookie-message"></div>
+  <div data-module="back-link"></div>
+  <div data-module="character-count"></div>
+  <div data-module="form"></div>
+  <div data-copy-course="warning"></div>
+</div>
+`
+
+    require("./app")
+  })
+
+  describe("initialising the app", () => {
+    it("should initialise govuk-frontend", () => {
+      expect(initAll).toHaveBeenCalled()
+    })
+
+    it("should initialise CookieMessage", () => {
+      expect(CookieMessage).toHaveBeenCalled()
+    })
+
+    it("should initialise BackLink", () => {
+      expect(BackLink).toHaveBeenCalled()
+    })
+
+    it("should initialise CharacterCount", () => {
+      expect(CharacterCount).toHaveBeenCalled()
+    })
+
+    it("should prevent user from navigating away", () => {
+      expect(window.onbeforeunload()).toBe("You have unsaved changes, are you sure you want to leave?")
+    })
+  })
+})
