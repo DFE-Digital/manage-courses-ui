a2361732446ce0951692d61a737548649f1c2ced
the-daniel-rothig <daniel.rothig@gmail.com>
2018-10-16 12:28:12 +0100

    Split out a file of view models to make code easier to navigate
    https://trello.com/c/lN9Z51QU/235-tidy-up-mange-course-ui-fromucasviewmodelcs

---
+++ src/ui/ViewModels/SchoolViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace GovUk.Education.ManageCourses.Ui.ViewModels
+{
+    public class SchoolViewModel
+    {
+        public string LocationName { get; set; }
+        public string Address { get; set; }
+        public string Code { get; set; }
+        public string Status { get; set; }
+        public string VacStatus { get; set; }
+
+        private string _applicationsAcceptedFrom;
+        public string ApplicationsAcceptedFrom
+        {
+            get => FormatDate(_applicationsAcceptedFrom);
+            set => _applicationsAcceptedFrom = value;
+        }
+
+        private static string FormatDate(string dateToFormat)
+        {
+            return DateTime.TryParse(dateToFormat, out var date) ? date.ToString("dd MMM yyyy") : dateToFormat;
+        }
+    }
+}
