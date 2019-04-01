5ca6945e4ee45a97268b0203b69ef18e75d2b800
Theodor Vararu <theo@vararu.org>
2019-04-01 16:21:22 +0100

    [1225] Use EnrichmentWorkflowStatus to display correct enrichment status
    The current statuses are not completely correct.

--- src/ui/Helpers/WorkflowStatusHelper.cs
+++ src/ui/Helpers/WorkflowStatusHelper.cs
@@ -8,20 +8,20 @@ namespace GovUk.Education.ManageCourses.Ui.Helpers
 {
     public static class WorkflowStatusHelper
     {
-        public static WorkflowStatus GetWorkflowStatus(this OrganisationViewModel model)
+        public static Ui.ViewModels.Enums.WorkflowStatus GetWorkflowStatus(this OrganisationViewModel model)
         {
             if (model.Status == EnumStatus.Published)
             {
-                return WorkflowStatus.Published;
+                return Ui.ViewModels.Enums.WorkflowStatus.Published;
             }
 
             var hasBeenPublishedBefore = model.LastPublishedTimestampUtc.HasValue && model.LastPublishedTimestampUtc > DateTime.MinValue;
             if (hasBeenPublishedBefore)
             {
-                return model.IsEmpty() ? WorkflowStatus.BlankSubsequentDraft : WorkflowStatus.SubsequentDraft;
+                return model.IsEmpty() ? Ui.ViewModels.Enums.WorkflowStatus.BlankSubsequentDraft : Ui.ViewModels.Enums.WorkflowStatus.SubsequentDraft;
             }
 
-            return model.IsEmpty() ? WorkflowStatus.Blank : WorkflowStatus.InitialDraft;
+            return model.IsEmpty() ? Ui.ViewModels.Enums.WorkflowStatus.Blank : Ui.ViewModels.Enums.WorkflowStatus.InitialDraft;
 
         }
     }
